00f59f3edae28067397da2960128c111a7f3f5ac
Mitchmer <themitchguy@gmail.com>
2025-10-08 18:20:32 -0700

    added barebones code for driver

--- driver.s
+++ driver.s
@@ -1,8 +1,8 @@
 //============================================================================
-//  Mitch Merrell
-//  CS3B - Title
-//  Date Created: XX/XX/XXXX
-//  Date Last Modified: XX/XX/XXXX
+//  Mitch Merrell, Osvaldo Medina Hernandez | Bin2Dec Project Group 5
+//  CS3B - Bin2Dec Group Project
+//  Date Created: 10/08/2025
+//  Date Last Modified: 10/08/2025
 //===== if just a function, don't include this next part =====================
 //  *** PROGRAM DESCRIPTION ****
 //  Algorithm/Pseudocode:
@@ -15,39 +15,10 @@
 _start:
     .EQU    SYS_EXIT, 93    // service code for program exit
 
-//****************************************************************************
-//  function title
-//============================================================================
-//  Function description
-//============================================================================
-//  Input: 
-//  Output: 
-//============================================================================
-//  Registers:
-//  X0: 
-//  X1:
-//  X2:
-//  X8:
-//****************************************************************************
-//  Function Algorithm/Pseudocode:
-//      ALGORITHM GOES HERE
-//============================================================================
+    MOV X0, #0              // Prepare a 0 return code
+    MOV X8, #SYS_EXIT       // Linux system call code for exit
+    SVC 0                   // Supervisor call to terminate program
+
     .data
 .end                        // code body end    
-
-// all functions/,acros must be documented with header comments that describe
-// functionality, inpuyts, outputs, and registers used (see lab descriptions
-// that require you to write functions for examples)
-
-// all labels must be left-aligned not tabbed in
-
-// the .global and .end directives must be left-aligned while all other
-// directives (e.g. .text, .data) must be tabbed once
-
-// All code is tabbed once (use 4 spaces for tabs)
-
-// Each line of code must be commented and instructions and registers must be 
-// upper case e.g. MOV X0, x1  not mov x0, x1)
-
-// Label/variable namnes must adhere to Hungarian Notation
  
