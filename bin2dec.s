51a788524ad9e0502185340c0ea1e52f1574394c
Mitchmer <themitchguy@gmail.com>
2025-10-21 15:43:10 -0700

    updated documentation for bin2dec.s

--- bin2dec.s
+++ bin2dec.s
@@ -2,16 +2,7 @@
 //  Mitch Merrell, Osvaldo Medina Hernandez | Bin2Dec Project Group 5
 //  CS3B - bin2dec function
 //  Date Created: 10/08/2025
-//  Date Last Modified: 10/09/2025
-//============================================================================
-
-.global _start
-    .text                   // code body start
-    .EQU SYS_EXIT, 93      // alias for the call code to terminate
-    .EQU BUFFER_SIZE, 64    // alias for the size of the buffer
-
-//****************************************************************************
-//  bin2dec function
+//  Date Last Modified: 10/21/2025
 //============================================================================
 //  Overview: Converts a given binary number, as a string, into its equivalent 
 //  decimal value and displays it to the console.
@@ -34,66 +25,44 @@
 //  Input: Calls getstring function to receive keyboard input from user
 //  Output: Calls putstring to display equivalent decimal value to the console
 //============================================================================
-//  Registers:
-//  X0: 
-//  X1:
-//  X2:
-//  X8:
-//****************************************************************************
 //  Function Algorithm/Pseudocode:
 //      1. Get input from user
 //          a. call getstring to retrieve up to 64 bits
 //      2. Process returned string
-//      3. Call checkkey function to analyze input string
-//          a. while input_length < 64
-//          b. check the current character and increment pointer
-//          c. if it's a '0' '1' AND number_length counter < 16:
-//              i. store the current character in the number string buffer
-//              ii. increment the number_length counter
-//          d. if it's a 'q':
-//              i. clear the input string
-//              ii. terminate the program
-//          e. if it's a 'c':
-//              i. clear the number string buffer
-//              ii. set the number_length counter to 0
-//          f. if it's an 'Enter':
-//              i. exit the loop
-//          g. increment input_length counter
-//          h. jump back to the beginning of length
+//      3. Call check_key() function to analyze and parse input string
+//      4. Check return code from check_key()
+//          a. 0: parse was successful, continue to number conversion
+//          b. 1: 'q' was found, jump to program termination
+//          c. Any other return code means the input was invalid
+//              i. jump back to beginning of program 
 //      5. Call bincstr2int with the number string as a parameter
-//      6. Call int2cstr and pass converted number as a parameter
-//      7. Check sign of number
-//      8. if it's negative:
+//      6. Check sign of number
+//      7. if it's negative:
 //          a. pass "->" to putstring
 //          b. otherwise, pass "->+" to putstring
-//      9. pass converted number to putstring
-//      10. wait for additional input + 'Enter'
-//          a. read input
-//          b. loop through string given
-//              i. check current char and increment pointer
-//              ii. if it's a 'c':
-//                  1. clear the input string
-//                  2. go back to the beginning of program
-//              iii. if it's a 'q':
-//                  1. terminate program
+//      8. Call int2cstr and pass converted number as a parameter
+//      9. pass converted number to putstring to display
+//      10. Call post_key() to wait for additional input + 'Enter'
+//      11. Check return code from post_key()
+//          a. 0: user wants to start over, go back to beginning of program
+//          b. 1: 'q' was entered, jump to end of program for termination
 //============================================================================
-_start:    
-    // get input from user
-    // call getstring to get user input
-input_loop:
-    LDR X0, =szInitialInputPrompt 
+
+.global _start                      // provide global access to program start point
+    .text                           // code body start
+    .EQU SYS_EXIT, 93               // alias for the call code to terminate
+    .EQU BUFFER_SIZE, 64            // alias for the size of the buffer
+
+_start:                             // program start point
+
+input_loop:                         // start of main input loop
+    LDR X0, =szInitialInputPrompt   // load initial input prompt
     BL putstring                    // display prompt
     LDR X0, =szBinaryBuffer         // prepare binary string buffer for getstring
-    BL getstring
-
+    BL getstring                    // get binary string from user
     LDR X0, =szBinaryBuffer         // load buffer into X0 for check_key input
     MOV X1, #BUFFER_SIZE            // prepare buffer size for check_key
     BL check_key                    // call check_key to process
-   
-    // 0 valid
-    // 1 quit
-    // 2 clear
-    // 3 invalid
     CMP X0, #0                      // check value returned from check_key
     B.EQ process_string             // if it's a zero, string is valid and can be processed
     CMP X0, #1                      // check value returned from check_key if not zero
@@ -111,60 +80,42 @@ process_string:                     // begin processing binary string
     LDR X0, =szPlus                 // else load a plus sign
     BL putstring                    // display the plus sign
 
-endif_negative:
+endif_negative:                     // end of if-else negative check 
     MOV X0, X19                     // prepare integer to convert to c-string
-    LDR X20, =szIntegerBuffer        // load integer output buffer 
+    LDR X20, =szIntegerBuffer       // load integer output buffer 
     MOV X1, X20                     // prepare buffer for int2cstr
     BL int2cstr                     // convert integer to cstring
-    MOV X0, X20                      // prepare output string for display
+    MOV X0, X20                     // prepare output string for display
     BL putstring                    // display number
     LDR X0, =szEOL                  // prepare newline character for display
     BL putstring                    // display newline character
 
-    // TODO : implement post key prompt and check
-
-post_loop:
+post_loop:                          // loop of post-conversion input
     LDR X0, =szPostKeyPrompt        // prepare post-conversion prompt
     BL putstring                    // display post-conversion prompt to user
-    LDR X0, =szBinaryBuffer        // prepare buffer to pass to post key
-    MOV X1, #BUFFER_SIZE
+    LDR X0, =szBinaryBuffer         // prepare buffer to pass to post_key
+    MOV X1, #BUFFER_SIZE            // prepare buffer size for post_key
     BL post_key                     // go to post key function
     CMP X0, #0                      // compare return code to 0
     B.EQ input_loop                 // if it's a 0, go back to beginning of program
                                     // otherwise, move to end of program
-end_program:                        
-    // end program
+
+end_program:                        // program end section
     MOV X0, #0                      // prepare return code 0
     MOV X8, #SYS_EXIT               // prepare system call code for program exit
     SVC 0                           // Linux supervisor call to terminate program
 
-    .data
+    .data                           // data section
 
 szTestString: .asciz "1000000000000000" // -32768
-szInitialInputPrompt: .asciz "Enter a sequence of binary digits, 'c' to clear, and/or 'q' to quit: "
-szPostKeyPrompt: .asciz "Enter a 'c' to start over, or 'q' to quit: "
-szArrow: .asciz "-> "
-szPlus: .asciz "+"
-szArrowBuffer: .skip 4
+szInitialInputPrompt: .asciz "Enter a sequence of binary digits, 'c' to clear, and/or 'q' to quit: " // initial input prompt
+szPostKeyPrompt: .asciz "Enter a 'c' to start over, or 'q' to quit: " // secondary input prompt
+szArrow: .asciz "-> "               // arrow for conversion display
+szPlus: .asciz "+"                  // sign for positive numbers
+szArrowBuffer: .skip 4              // buffer to store arrow + sign
 szBinaryBuffer: .skip BUFFER_SIZE   // buffer for string input
 szIntegerBuffer: .skip BUFFER_SIZE  // buffer for integer output
 szPostKeyBuffer: .skip 4            // buffer to pass to post key for user input
 szEOL: .asciz "\n"                  // newline character for display 
 
 .end                                // code body end    
-
-// all functions/,acros must be documented with header comments that describe
-// functionality, inpuyts, outputs, and registers used (see lab descriptions
-// that require you to write functions for examples)
-
-// all labels must be left-aligned not tabbed in
-
-// the .global and .end directives must be left-aligned while all other
-// directives (e.g. .text, .data) must be tabbed once
-
-// All code is tabbed once (use 4 spaces for tabs)
-
-// Each line of code must be commented and instructions and registers must be 
-// upper case e.g. MOV X0, x1  not mov x0, x1)
-
-// Label/variable namnes must adhere to Hungarian Notation
